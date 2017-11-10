a669e9d9d73ca34a1ce81922ca9c68584086f634
ethanryan <ethanryan@gmail.com>
2017-11-10 17:11:43 -0500

    added script_serializer

---
+++ app/serializers/script_serializer.rb
@@ -0,0 +1,17 @@
+class ScriptSerializer < ActiveModel::Serializer
+  attributes :id,
+             :user_id,
+             :title,
+             :content,
+
+             :user,
+
+             :genres,
+             :plots,
+             :scenes,
+             
+             :characters,
+end
+
+#to make serializer, in rails terminal:::
+#rails g serializer [name_of_model_making_serializer_for]
